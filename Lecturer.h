aa92587d1ece6a8c47cd2b8e2ca8ab54a120d3f3
Hồ Khánh Duy <68040131+chulanpro5@users.noreply.github.com>
2022-03-18 11:31:10 -0700

    create Lecturer and Staff

---
+++ Lecturer.h
@@ -0,0 +1,13 @@
+struct Lecturer {
+    string username;
+    string password;
+ 
+    string name;
+    string id;
+    string email;
+ 
+    void viewCourses();
+ 
+    // import scoreboard
+    // ... some more
+};
---
+++ Staff.h
@@ -0,0 +1,20 @@
+struct Staff {
+ 
+    string username;
+    string password;
+ 
+    string name;
+    string id;
+    string email;
+ 
+    // các phương thức của 1 sinh viên
+ 
+    // At any time, an academic staff member can:
+ 
+    void viewClasses();
+ 
+    void viewCourses();
+ 
+	Void updateResult(); // friend ???
+ 
+};
