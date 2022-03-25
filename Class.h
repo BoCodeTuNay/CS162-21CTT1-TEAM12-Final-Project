aafaebb671bd70beedbdb6295661f62d83b33171
Le Viet <leviettt.tnh@gmail.com>
2022-03-25 20:31:32 +0700

    Update Class.h

--- Class.h
+++ Class.h
@@ -1,5 +1,13 @@
 #include "Student.h"
 
+struct StudentNode {
+    Student *mem;
+    StudentNode *next;
+};
+
 struct Class {
-    
+    StudentNode* stu;
+    int Student_num = 0;
+
+    void Add_Student();
 };
