f062894b08898240eaded2aec96a53601cc3dcf5
Le Viet <leviettt.tnh@gmail.com>
2022-03-31 21:22:42 +0700

    Update Student.h

---
+++ Student/Student.h
@@ -0,0 +1,30 @@
+#include "List.h"
+#include "Course.h"
+
+struct Score {
+    double mid_term_p;
+    double process_p;
+    double final_p;
+}
+
+struct CourseScore {
+    Course *pCourse;
+    Score *pScore;
+}
+
+struct Student {
+    char username[20];
+    char password[20];
+
+    char name[50];
+    int id;
+    char email[100];
+
+    List<CourseScore> pScore;
+
+    void enrolledCourse();
+
+    void viewEnrolledCourses();
+
+    void removeCourse();
+}
