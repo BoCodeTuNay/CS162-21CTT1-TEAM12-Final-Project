136f3f2a5670982a4c404cad144bc7b777c709bf
Le Viet <leviettt.tnh@gmail.com>
2022-04-13 02:24:22 +0700

    Update Student.cpp

--- Student/Student.cpp
+++ Student/Student.cpp
@@ -2,15 +2,15 @@
 #include "Student.h"
 
 bool cmp_Course(const CourseScore &a, const CourseScore &b) {
-    return (a.pCourse == b.pCourse);
+    return (a.pCourse.id == b.pCourse.id);
 }
 
 void Student::enrolledCourse(List<Course> pOpenCourse) {
     if (pScore.size() > 4) {
         /// Thông báo sinh viên không thể đăng ký thêm
     } else {
-        Course* pickCourse = nullptr;
-        Score* c = new Score;
+        Course pickCourse;
+        Score c;
         for (Node<Course>* p = pOpenCourse.begin(); p; p = p -> pNext) {
             /// Hiển thị danh sách cho sinh viên chọn khóa đăng ký thêm.
         }
@@ -30,7 +30,10 @@ void Student::removeCourse(string id) {
     CourseScore pick;
 
     for (Node<CourseScore>* p = pScore.begin(); p; p = p -> pNext) {
-        /// Hiển thị danh sách sinh viên đã enroll
+        if (p -> data.pCourse.id == id) {
+            pick = p->data;
+            break;
+        }
     }
 
     pScore.remove(pick, cmp_Course);
