da89fb61c31c1d51c1c25c42a6edf06b4029a3cb
vokhang1412 <vokhang1412@gmail.com>
2022-04-15 07:49:25 +0700

    Update Semester

--- Semester/Semester.h
+++ Semester/Semester.h
@@ -3,15 +3,30 @@
 #include "..\Course\Course.h"
 
 struct Semester {
-    int ID; // 1, 2, or 3
+    bool created = false;
     Date start_date;
     Date end_date;
+    Date start_registration_date;
+    Date end_registration_date;
     List<Course> listCourses;
  
     // load a Semester from the given stream (the stream will be opened and closed outside this function, so don't need to do it)
-    void load_data(ifstream&);
+    //void load_data(ifstream&);
+
     // create a Course Registration Session, this feature might need further discussion...
     void createCourseRegistration();
 };
 
-#include "Semester.cpp"
+/*
+    
+
+format của 1 khối Semester (dự kiến):
+<start_date>
+<end_date>
+K // số lượng Course đang có hiện tại
+<1st Course> // một khối dữ liệu của 1 Course
+<2nd Course>
+...
+<Kth Course>
+    
+*/
