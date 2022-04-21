ccdd2cd9eb2de486d2b2bada9654b6713f51cef1
Duy Hồ <chulanpro5@gmail.com>
2022-04-21 19:24:11 +0700

    add courseInfo

---
+++ Course/CourseInfo.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "..\\Student\\Student.h"
+#include "..\\Date\\Date.h"
+#include "Profile/Profile.h"
+#include "Score.h"
+
+struct CourseInfo
+{
+    char ID[MAXSTR+1]; // mã học phần
+    char name[MAXSTR+1]; // tên học phần
+};
