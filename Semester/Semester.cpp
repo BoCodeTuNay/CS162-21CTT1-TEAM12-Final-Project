6d08c5489568401b75d4fd42fd6639a45d2a311d
vokhang1412 <vokhang1412@gmail.com>
2022-04-22 23:15:03 +0700

    Update Semester.cpp

--- Semester/Semester.cpp
+++ Semester/Semester.cpp
@@ -1,12 +1,15 @@
 #include "Semester.h"
 
 void Semester::createCourseRegistration(List <CourseInfo>& listCourseInfo) {
-    CourseInfo course;
+    CourseInfo courseInfo;
     int check;
     do {
-        course.enterCourseInfo();
+        courseInfo.enterCourseInfo();
         cout << "Are you sure about all of the information of the course you want to create? (0 for No and 1 for Yes): ";
         cin >> check;
     } while (check == 0);
-    listCourseInfo.insert(course);
+    listCourseInfo.insert(courseInfo);
+    Course course;
+    course.courseInfo = courseInfo;
+    listCourses.insert(course);
 }
